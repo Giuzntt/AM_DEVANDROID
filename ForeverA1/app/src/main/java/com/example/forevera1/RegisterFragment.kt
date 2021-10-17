ae2f18e6714986beeb091f7f88c894c295d9956e
Giuzntt <giuzntt@gmail.com>
2021-10-17 18:33:23 -0300

    ..

--- ForeverA1/app/src/main/java/com/example/forevera1/RegisterFragment.kt
+++ ForeverA1/app/src/main/java/com/example/forevera1/RegisterFragment.kt
@@ -19,6 +19,7 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding?.root
+        //BINDING
     }
 
 
